d1470f781c6a261db5f0cb1fe140589255ff9e5e
Rajdeep Rudra <rajdeeprudra2003@gmail.com>
2026-04-23 23:35:45 +0530

    added entry point to backend

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+import express from 'express';
+
+const app = express();
+
+const PORT = 4000;
+
+app.get("/health", (req,res)=>{
+    res.json({
+        msg:"ALL OK NIGGA MOVE!"
+    })
+})
+
+
+app.listen(PORT);
+
