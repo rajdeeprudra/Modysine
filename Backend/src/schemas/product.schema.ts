f1446d6849c9f99c6da89b6ca327d6a60d878dae
Rajdeep Rudra <rajdeeprudra2003@gmail.com>
2026-04-25 15:00:47 +0530

    added the zod schema for products

--- Backend/src/schemas/product.schema.ts
+++ Backend/src/schemas/product.schema.ts
@@ -1,8 +1,53 @@
 // src/schemas/product.schema.ts
-import { string, z } from 'zod';
+import { z } from 'zod';
 
 export const productSchema = z.object({
-    body: z.object({
-        name : string().min(3)
+    body:z.object({
+        name : z.string("name is required")
+                      .trim()
+                      .min(6)
+                      .regex(/^[a-zA-Z\s]+$/, "Name can only contain Letters and speaces")
+                      //REFINE: Custom logic to block repeating spam characters like "cccccc"
+                      .refine((val)=>{
+
+                        //removing the unnecessary spaces
+                        const cleanString = val.replace(/\s/g,'');
+
+                        if(cleanString.length==0) return false;
+
+                        const firstCharacter = cleanString[0];
+                        const isAllSameCharacter = cleanString.split("").every(char=> char == firstCharacter);
+
+                        return !isAllSameCharacter;
+                      },{ message: "Please enter a valid real name for the Product!" }),
+
+        description : z.string("description is required")
+                       .trim()
+                       .min(10, "description must be atleast of 10 characters")
+                       .refine((val)=>{
+
+                        //removing the unnecessary spaces
+                        const cleanString = val.replace(/\s/g,'');
+
+                        if(cleanString.length==0) return false;
+
+                        const firstCharacter = cleanString[0];
+                        const isAllSameCharacter = cleanString.split("").every(char=> char == firstCharacter);
+
+                        return !isAllSameCharacter;
+                      },{ message: "Please enter a valid real name for the Product!" }), 
+        images: z.array(
+                    z.url("Each Image must be a valid url")
+                    ).min(1, "You must add atleast 1 image url"),
+
+        price: z.number()
+               .positive("Price must be a positive number"),
+        
+        isActive: z.boolean().optional(),
     })
-})
+        
+        
+});
+
+// inferred type to use it in controllers
+export type productInput = z.infer<typeof productSchema>['body'];
